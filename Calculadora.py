63fe22ea3aafb395d57b860a385a291ff0b4b5ea
Palta xd <raf.albornoz@duocuc.cl>
2025-05-11 23:40:02 -0400

    Update Suma

---
+++ Calculadora.py
@@ -0,0 +1,26 @@
+while True:
+    try:
+        num1 = int(input("Ingrese numero 1: "))
+        num2 = int(input("Ingrese numero 2: "))
+        menu = '''
+               1. Sumar
+               5. Salir
+           '''
+        
+        print(menu)
+        op = int(input("Eliga un Numero por favor: "))
+
+        if op == 1:
+            print("La suma es: ",num1+num2)
+            continue;
+        if op == 5:
+            print("Gracias por usar la calculadora, adios")
+            break;
+        else:
+            print("Debe ser un Numero entre 1 y 5")
+    except ZeroDivisionError:
+        print("No se puede dividir entre 0")
+    except ValueError:
+        print("Debe ser un valor Numerico")
+    except Exception as error:
+        print("Hay un error: ",error)
